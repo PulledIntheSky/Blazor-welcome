3703970b0800bd063c1524a2288dcf94a65a7f3d
PulledIntheSky <75171834+PulledIntheSky@users.noreply.github.com>
2024-06-06 14:05:43 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# Adjust ping_group_range for the running user
-echo "0 65535" > /proc/sys/net/ipv4/ping_group_range
+# Try to adjust ping_group_range for the running user if possible
+if [ -w /proc/sys/net/ipv4/ping_group_range ]; then
+    echo "0 65535" > /proc/sys/net/ipv4/ping_group_range || echo "Warning: Unable to set ping_group_range, proceeding without it."
+else
+    echo "Warning: /proc/sys/net/ipv4/ping_group_range is not writable, proceeding without setting ping_group_range."
+fi
 
 # Start the Cloudflared tunnel and serve the index.html file
 exec cloudflared tunnel --config /usr/local/etc/cloudflared/config.yml --url http://localhost:80 --no-autoupdate true
