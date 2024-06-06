283e7d5f7420aa7c759e6208c3a1982ed8ae9b77
PulledInTheSky <nobodyrevange@hotmail.com>
2024-06-06 16:44:02 +0200

    some changes

--- server.cjs
+++ server.cjs
@@ -1,5 +1,5 @@
 const express = require('express');
-const fetch = require('node-fetch'); // Import the node-fetch library
+const fetch = require('node-fetch').default;; // Import the node-fetch library
 
 const app = express();
 const PORT = process.env.PORT || 3000;
