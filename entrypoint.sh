58aee256d33467ef5bf7b74b9a89f7738b76c0b4
PulledIntheSky <75171834+PulledIntheSky@users.noreply.github.com>
2024-06-06 14:08:33 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# Try to adjust ping_group_range for the running user if possible
-if [ -w /proc/sys/net/ipv4/ping_group_range ]; then
-    echo "0 65535" > /proc/sys/net/ipv4/ping_group_range || echo "Warning: Unable to set ping_group_range, proceeding without it."
-else
-    echo "Warning: /proc/sys/net/ipv4/ping_group_range is not writable, proceeding without setting ping_group_range."
-fi
+# Start Cloudflare Warp
+warp-cli --accept-tos connect
+
+# Wait for Warp to establish the connection
+sleep 5
 
 # Start the Cloudflared tunnel and serve the index.html file
 exec cloudflared tunnel --config /usr/local/etc/cloudflared/config.yml --url http://localhost:80 --no-autoupdate true
